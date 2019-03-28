26e615ecc69c96855c099e1537944998d423a800
Segun Akanbi <akanbisegun1@gmail.com>
2019-03-28 07:18:30 +0100

    [Chore #164940292] Responsive homepage

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -4,3 +4,13 @@ function modalOpen(bool) {
 	bool ? modal.style.display = 'block' :
 		   modal.style.display = 'none';
 }
+
+// Hamburger Menu
+
+function openSlideMenu() {
+	document.getElementById('side-menu').style.width = '300px';
+}
+
+function closeSlideMenu() {
+	document.getElementById('side-menu').style.width = '0';
+}
