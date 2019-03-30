75aff3f46aae33128f3c4ae7dc99fc4d0b0d31ec
Segun Akanbi <akanbisegun1@gmail.com>
2019-03-30 09:31:13 +0100

    [Chore] Setup server file (app entry point)

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { usersRoute } from './server/routes';
+
+const app = express();
+app.set('port', process.env.PORT || 5000);
+
+app.get('/api/v1', (req, res) => {
+	res.status(200).send({
+		status: 200,
+		data: 'Welcome to Banka API',
+	});
+});
+
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// append /api to http requests
+app.use('/api/v1', [usersRoute]);
+
+app.listen(app.get('port'), () => console.log(`LISTENING ON PORT ${app.get('port')}`));
