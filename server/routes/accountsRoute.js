c4d6b3aff6d2478218be5a34949166a74299b8b3
Segun Akanbi <akanbisegun1@gmail.com>
2019-04-28 21:12:12 +0100

    feature(endpoint):active-account -view active/dormant accounts [Finishes #165437244]

--- server/routes/accountsRoute.js
+++ server/routes/accountsRoute.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router
   .get('/accounts/:accountNumber', Auth.tokenVerify, AccountsController.findOne)
-  .get('/accounts', /*Auth.tokenVerify, Admin.nonStaff,*/ AccountsController.findAll)
+  .get('/accounts', Auth.tokenVerify, Admin.nonStaff, AccountsController.findAll)
   .get('/accounts/:accountNumber/transactions', Auth.tokenVerify, TransactionsController.findAll)
   .post('/transactions/:accountNumber/credit', Auth.tokenVerify, AccountsController.creditAccount)
   .post('/transactions/:accountNumber/debit', Auth.tokenVerify, AccountsController.debitAccount)
